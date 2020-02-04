c2708d1832f58c5291d160096a9734cdf697dba5
MUHAMMAD Siyab <siyab.dev@gmail.com>
2020-02-04 21:52:59 +0500

    Added JS directory

---
+++ JS/data_types.js
@@ -0,0 +1,38 @@
+// Variables
+var message = "Hello World";
+
+console.log(message);
+
+// String
+var username = "John312";
+
+// Number
+var age = 34.7; //NaN
+
+console.log(typeof age);
+
+// Undefined
+console.log(name.toUpperCase());
+
+// Null
+var skills = null; // No value
+console.log(typeof skills);
+
+// Boolean
+var married = true;
+
+console.log(typeof married);
+
+// Object / Arrays
+var names = ["John", "Sara", "Kim"];
+
+console.log(typeof names);
+
+// Object / JSON Object
+var user = {
+  name: "Brad",
+  age: 23,
+  married: false
+};
+
+console.log(user.age);
