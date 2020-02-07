24df9e1650bddd90e1f03239c5a5462afc2645bf
MUHAMMAD Siyab <siyab.dev@gmail.com>
2020-02-07 21:33:03 +0500

    Added conditions.js

---
+++ JS/conditions.js
@@ -0,0 +1,33 @@
+var x = 2;
+var y = 5;
+
+if (x === y) {
+  console.log("X is equal to Y");
+} else if (x < y) {
+  console.log("X is greater than Y");
+} else {
+  console.log("Nothing matches");
+}
+
+var fruit = "mango";
+
+// Template Literals
+
+// Switch
+switch (fruit) {
+  case "orange":
+    console.log(`This fruit is ${fruit}`);
+    break;
+
+  case "apple":
+    console.log(`This fruit is ${fruit}`);
+    break;
+
+  case "banana":
+    console.log(`This fruit is ${fruit}`);
+    break;
+
+  default:
+    console.log(`This fruit is ${fruit}`);
+    break;
+}
